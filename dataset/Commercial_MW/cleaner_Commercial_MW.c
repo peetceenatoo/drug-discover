f71fc7b9075bb8e1956e2decb0cd33f24028049d
Mattia <piccinatomattia@gmail.com>
2023-05-08 15:52:04 +0200

    rearranged cleaners code

--- dataset/Commercial_MW/cleaner_Commercial_MW.c
+++ dataset/Commercial_MW/cleaner_Commercial_MW.c
@@ -12,51 +12,68 @@
 #define NEW3 "Commercial_MWhigher500(clean).csv"
 
 int main(){
+
+  // Open the files
   FILE* old1 = fopen(OLD1, "r");
   FILE* old2 = fopen(OLD2, "r");
   FILE* old3 = fopen(OLD3, "r");
-
   FILE* new1 = fopen(NEW1, "w");
   FILE* new2_1 = fopen(NEW2_1, "w");
   FILE* new2_2 = fopen(NEW2_2, "w");
   FILE* new3 = fopen(NEW3, "w");
   if(old1 == NULL || old2 == NULL || old3 == NULL || new1 == NULL || new2_1 == NULL || new2_2 == NULL || new3 == NULL) return -1;
+
   char temp;
   bool smile;
 
-  smile = true;
-  while( (temp = getc(old1)) != '\n');
-  while( (temp = getc(old1)) != EOF){
-    if(temp == ',') smile=false;
-    if(temp == '\n') smile=true;
-    if(smile) fprintf(new1, "%c", temp);
+  // The flag smiles will be true until the first comma is reached, and then be false until the \n is reached
+
+  // File old1 -> new1
+  smiles = true;
+  // Skip first row
+  while( ( temp = getc(old1) ) != '\n' );
+  // Read all the smiles
+  while( ( temp = getc(old1) ) != EOF ){
+    if( temp == ',' ) smiles = false;
+    else if( temp == '\n' ) smiles = true;
+    if( smiles ) fprintf(new1, "%c", temp);
   }
 
-  smile = true;
-  while( (temp = getc(old2)) != '\n');
-  int count = 1407638; //half of the file rows
-  while( (temp = getc(old2)) != EOF){
-    if(temp == ',') smile=false;
-    if(temp == '\n') {
-      smile=true;
+  // Currently half of the rows in the dataset "Commercial_MW330-500.csv"
+  // Need to calculate count runtime soon
+  int count = 1407638;
+
+  // File old2 -> new2_1 and new2_2
+  smiles = true;
+  // Skip first row
+  while( ( temp = getc(old2) ) != '\n' );
+  // Read all the smiles
+  while( ( temp = getc(old2) ) != EOF ){
+    if( temp == ',' ) smiles = false;
+    else if( temp == '\n' ){
+      smiles = true;
       count--;
       }
-    if(smile && count>0) fprintf(new2_1, "%c", temp);
-    if(smile && count<=0) fprintf(new2_2, "%c", temp);
+    // If half of the file is reached, print in new2_1, else in new2
+    if( smiles && ( count>0 ) ) fprintf(new2_1, "%c", temp);
+    else if( smiles && ( count<=0 ) ) fprintf(new2_2, "%c", temp);
   }
 
-  smile = true;
+  // File old3 -> new3
+  smiles = true;
+  // Skip first row
   while( (temp = getc(old3)) != '\n');
-  while( (temp = getc(old3)) != EOF){
-    if(temp == ',') smile=false;
-    if(temp == '\n') smile=true;
-    if(smile) fprintf(new3, "%c", temp);
+  // Read all the smiles
+  while( ( temp = getc(old3) ) != EOF ){
+    if( temp == ',' ) smile = false;
+    else if( temp == '\n' ) smile = true;
+    if( smile ) fprintf(new3, "%c", temp);
   }
 
+  // Close the files
   fclose(old1);
   fclose(old2);
   fclose(old3);
-
   fclose(new1);
   fclose(new2_1);
   fclose(new2_2);
--- dataset/Drugs/cleaner_Drugs.c
+++ dataset/Drugs/cleaner_Drugs.c
@@ -5,19 +5,32 @@
 #define NEW "Drugs(clean).csv"
 
 int main(){
+
+  // Open the files
   FILE* old = fopen(OLD, "r");
   FILE* new = fopen(NEW, "w");
-  if(old == NULL || new == NULL) return -1;
+  if( ( old == NULL ) || ( new == NULL ) ) return -1;
+
   char temp;
   bool smile;
 
+  // The flag smiles will be true until the first comma is reached, and then be false until the \n is reached
+
+
   smile = true;
-  while( (temp = getc(old)) != '\n');
-  while( (temp = getc(old)) != EOF){
-    if(temp == ',') smile=false;
-    if(temp == '\n') smile=true;
-    if(smile) fprintf(new, "%c", temp);
+  // Skip first row
+  while( ( temp = getc(old) ) != '\n' );
+  // Read all the smiles
+  while( ( temp = getc(old) ) != EOF ){
+    if( temp == ',' ) smile = false;
+    if( temp == '\n' ) smile = true;
+    if( smile ) fprintf(new, "%c", temp);
   }
+
+  // Close the files
+  fclose(old);
+  fclose(new);
+
 
   return 0;
 }
