39d0385f625f88a9804d34c610563df16e52f25b
FraRita <francescorita2709@gmail.com>
2023-05-11 15:21:18 +0200

    Update code_fingerprint_generator.py

--- dataset/Commercial_MW/code_fingerprint_generator.py
+++ dataset/Commercial_MW/code_fingerprint_generator.py
@@ -9,20 +9,14 @@ from FPSim2.io import create_db_file
 if __name__ == '__main__':
 
     # Specify the paths for the database
-    path1 = "Commercial_MWlower330(changed).csv"
-    path2 = "Commercial_MW330-500(changed)1.csv"
-    path3 = "Commercial_MW330-500(changed)2.csv"
-    path4 = "Commercial_MWhigher500(changed).csv"
-    out1 = "Commercial_MWlower330(changed).h5"
-    out2 = "Commercial_MW330-500(changed)1.h5"
-    out3 = "Commercial_MW330-500(changed)2.h5"
-    out4 = "Commercial_MWhigher500(changed).h5"
+    path1 = "Commercial_MWlower330.csv"
+    path2 = "Commercial_MW330-500-1.csv"
+    path3 = "Commercial_MW330-500-2.csv"
+    path4 = "Commercial_MWhigher500.csv"
+    out1 = "fingerprints.h5"
 
     # List of input smiles strings
-    input_smiles1 = []
-    input_smiles2 = []
-    input_smiles3 = []
-    input_smiles4 = []
+    input_smiles = []
 
     # Open the files
     f1 = open(path1,"r")
@@ -35,25 +29,22 @@ if __name__ == '__main__':
     # Read all the smiles from f1
     i=1
     for x in f1:
-        input_smiles1.append([x.replace("\n",""),0+i])
+        input_smiles.append([x.replace("\n",""),0+i])
         i += 1
 
     # Read all the smiles from f2
-    i=1
     for x in f2:
-        input_smiles2.append([x.replace("\n",""),0+i])
+        input_smiles.append([x.replace("\n",""),0+i])
         i += 1
 
     # Read all the smiles from f3
-    i=1
     for x in f3:
-        input_smiles3.append([x.replace("\n",""),0+i])
+        input_smiles.append([x.replace("\n",""),0+i])
         i += 1
 
     # Read all the smiles from f4
-    i=1
     for x in f4:
-        input_smiles4.append([x.replace("\n",""),0+i])
+        input_smiles.append([x.replace("\n",""),0+i])
         i += 1
 
     #close the files
@@ -64,10 +55,4 @@ if __name__ == '__main__':
 
     print("Start creating the database... Good luck!")
 
-    try:
-        create_db_file(input_smiles1,out1,'Morgan',{'radius': 3,'nBits': 2048})
-        create_db_file(input_smiles2,out2,'Morgan',{'radius': 3,'nBits': 2048})
-        create_db_file(input_smiles3,out3,'Morgan',{'radius': 3,'nBits': 2048})
-        create_db_file(input_smiles4,out4,'Morgan',{'radius': 3,'nBits': 2048})
-    except Exception as e:
-        pass
+    create_db_file(input_smiles,out1,'Morgan',{'radius': 3,'nBits': 2048})
