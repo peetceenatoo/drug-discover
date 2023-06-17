d9bc2722ae3fc59ac67ad443bfc59320c9a7b780
FraRita <francescorita2709@gmail.com>
2023-06-17 11:15:44 +0200

    Update distributions_plotter.py

--- data analysis/features_distribution_plots/distributions_plotter.py
+++ data analysis/features_distribution_plots/distributions_plotter.py
@@ -18,6 +18,7 @@ logging.getLogger('tensorflow').setLevel(logging.FATAL)
 import matplotlib.pyplot as plt
 from molecule_generation import load_model_from_directory
 import random
+import copy
 
 # ------------------------------- Code ------------------------------- #
 
@@ -25,16 +26,20 @@ import random
 # due to molecule_generation package usage
 if __name__ == '__main__':
 
+    # Define the number of molecule to analyse
+    num_of_chosen_molecules = 100
+    # Define the interval length
+    interval = 0.02
+
     # Specify the directory of the trained model
-    model_dir = "model"
+    model_dir = "..\\..\\model"
 
     # Specify the paths for the database
-    path1 = "..\\dataset\\Commercial_MW\Commercial_MWlower330(clean).csv"
-    path2 = "..\\dataset\\Commercial_MW\Commercial_MW330-500(clean)1.csv"
-    path3 = "..\\dataset\\Commercial_MW\Commercial_MW330-500(clean)2.csv"
-    path4 = "..\\dataset\\Commercial_MW\Commercial_MWhigher500(clean).csv"
-    out1 = "chosen_molecules.csv"
-    out2 = "features_max_min.csv"
+    path1 = "..\\..\\dataset\\Commercial_MW\Commercial_MWlower330(clean).csv"
+    path2 = "..\\..\\dataset\\Commercial_MW\Commercial_MW330-500(clean)1.csv"
+    path3 = "..\\..\\dataset\\Commercial_MW\Commercial_MW330-500(clean)2.csv"
+    path4 = "..\\..\\dataset\\Commercial_MW\Commercial_MWhigher500(clean).csv"
+    out1 = "features_distributions.txt"
 
     # Open the files
     f1 = open(path1,"r")
@@ -53,7 +58,7 @@ if __name__ == '__main__':
     for x in f3:
         input_smiles.append(x.replace("\n",""))
     for x in f4:
-        input_smiles.append(x.replace("\n",""))
+        input_smiles.append(x.replace("\n",""))    
 
     # Close the files
     f1.close()
@@ -61,96 +66,77 @@ if __name__ == '__main__':
     f3.close()
     f4.close()
 
-    # Pick num_of_molecules_to_plot random smiles
-    num_of_molecules_to_plot = 100
+    # Pick num_of_chosen_molecules random smiles
     random.shuffle(input_smiles)
-    chosen_smiles = input_smiles[0:num_of_molecules_to_plot]
-
-    # Open the output file to store all the num_of_molecules_to_plot smiles
-    fout = open(out1,"w")
-
-    # Write on the output file
-    for c in chosen_smiles:
-        fout.write("{}".format(c))
-
-    # Close the output file
-    fout.close()
+    chosen_smiles = input_smiles[0:num_of_chosen_molecules]
 
     # Using the model at model_dir path
     with load_model_from_directory(model_dir) as model:
         print()
 
         # Process latent vector for each input smiles string
-        for i in range(len(chosen_smiles)):
-            embeddings = model.encode([chosen_smiles[i]])
+        embeddings = model.encode(chosen_smiles)
 
-        # Set number of embedding features
-        num_of_features = len(embeddings[0])
+    # Set number of embedding features
+    num_of_features = len(embeddings[0])
+
+    # Lists of min and max for all features
+    min = copy.deepcopy(embeddings[0])
+    max = copy.deepcopy(embeddings[0])
+
+    for i in range(1,num_of_chosen_molecules):
+        for j in range(num_of_features):
+            if embeddings[i][j] < min[j]:
+                min[j] = embeddings[i][j]
+            if embeddings[i][j] > max[j]:
+                max[j] = embeddings[i][j]
+
+    # The module of the magnitude of interval + 1
+    magnitude = 0
+    factor = 1
+    while interval*factor < 10 :
+        magnitude +=1
+        factor = factor * 10
+
+    # Open the output file to store all the num_of_chosen_molecules smiles
+    fout = open(out1,"w")
+    fout.write("{}\n".format(interval))
+
+    for i in range(num_of_features):
 
         # Init x-axis data
-        lower = -1
-        upper = 1
-        jump = 0.05
-        # Magnitude must equal reciprocal( min( magnitude(jump), magnitude(lower), magnitude(upper) ) )4
-        # Needed because range() only works with integer parameters
-        magnitude = 100
+        lower = round(magnitude, min[i] - min[i]%interval + interval/2)
+        upper = round(magnitude, max[i] - max[i]%interval + interval/2)
+        fout.write("{}:".format(lower - interval/2))
 
         # Define x-axis
-        x = list( range( (int)(lower*magnitude), (int)(upper*magnitude+1), (int)(jump*magnitude) ) )
+        x = list( range( (int)(lower*factor), (int)(upper*factor+1), (int)(interval*factor) ) )
         for i in range(len(x)):
-            x[i] = x[i]/magnitude
-        x.insert(0, lower - jump/2)
-        x.append(upper + jump/2)
+            x[i] = x[i]/factor
 
-        # Write the names of the CSV file columns
-        fout = open(out2,"w")
-        fout.write("feature,max,min\n")
+        # Init y-axis as list of len(x) elements
+        y = [0]*len(x)
 
-        # For every feature (from 0 to num_of_features-1)
-        for i in range(num_of_features):
+        for j in range(num_of_chosen_molecules):
 
-            # Init y-axis as list of len(x) elements
-            y = [0]*len(x)
-            # Init min and max
-            min = embeddings[0][i]
-            max = embeddings[0][i]
+            # Calculate the value on the y-axis
+            k=0
+            while embeddings[j][i] >= x[k]-interval/2 and k<len(x):
+                k += 1
+            y[k-1] += 1 
 
-            # For each smiles, eventually save the current smiles (j-th) feature as min or max
-            for j in range(num_of_molecules_to_plot):
-
-                # Update max and min
-                if embeddings[j][i] < min:
-                    min = embeddings[j][i]
-                if embeddings[j][i] > max:
-                    max = embeddings[j][i]
-
-                # If the current feature for the current smiles is to the left of the range on the x-axis
-                if embeddings[j][i] < x[0]:
-                    # Plot one more molecule on the left bond
-                    y[0] += 1
-                # If the current feature for the current smiles is to the right of the range on the x-axis
-                elif embeddings[j][i] >= x[len(x)-1]:
-                    # Plot one more molecule on the right bond
-                    y[len(x)-1] += 1
-                # Otherwise, plot one more molecule at the right value on the x-asis
-                else:
-                    # Calculate the value on the x-axis
-                    k=1
-                    while embeddings[j][i] >= x[k]-jump/2 and k<len(x)-1:
-                        k += 1
-                    # Plot one more
-                    y[k-1] += 1 
-
-            # Write max and min for the current feature (i-th)
-            fout.write("{},{},{}\n".format(i,max,min))
-
-            # Plot
-            plt.plot(x,y)
-            plt.xlabel('feature value')
-            plt.ylabel('feature count')
-            plt.title("feature n.{}".format(i))
-            plt.savefig("features_distribution_plots\\feature{}.png".format(i))
-            plt.clf()
-
-        # Close the output file
-        fout.close()
+        # Set the data in percentage
+        for j in range(len(y)):
+            y[j] = y[j]/num_of_chosen_molecules
+            fout.write("{};".format(y[j]))
+        fout.write("\n")
+        
+        # Plot
+        plt.plot(x,y)
+        plt.xlabel('feature values')
+        plt.ylabel('feature counts in percentage')
+        plt.title("feature n.{}".format(i+1))
+        plt.savefig("feature{}.png".format(i+1))
+        plt.clf()
+    
+    fout.close()
