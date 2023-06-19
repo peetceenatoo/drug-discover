d1df37c558330b2ac263d5dee3308767476b4f01
FraRita <francescorita2709@gmail.com>
2023-06-19 17:46:31 +0200

    Update FR-generator-attempt.py

--- data analysis/exploration_parameters/FR-generator-attempt.py
+++ data analysis/exploration_parameters/FR-generator-attempt.py
@@ -6,7 +6,6 @@
 #     the script and will be fixed by Microsoft team before the deprecated
 #     functions will have been deleted.
 
-from math import sqrt
 import os
 import logging
 from tensorflow.python.util import deprecation
@@ -29,10 +28,9 @@ from molecule_generation import load_model_from_directory
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit import DataStructs
-import numpy as np
+from FPSim2 import FPSim2Engine
 import os
 import random
-import time
 import copy
 
 # ------------------------------ Global ------------------------------- #
@@ -44,12 +42,7 @@ distributions = []
 
 # ------------------------------ addNoise ------------------------------- #
 
-def addNoise(embedding, area, num_of_molecules, num=None):
-
-    # If the number of elements to add disturb to is not explicitly defined,
-    # then add noise to the whole embedding
-    if num == None:
-        num = len(embedding)
+def addNoise(embedding, area):
 
     global interval_size
     global magnitude
@@ -120,29 +113,18 @@ def addNoise(embedding, area, num_of_molecules, num=None):
                 current_area += odd/interval_size*(p_lower + p_upper)
             j += 1
         
-        r = sqrt((area-current_area)*interval_size/(p_lower+p_upper))
+        r = (area-current_area)*interval_size/(p_lower+p_upper)
         noise_intervals_lengths.append(upper_bound-lower_bound+2*r)
-        
-    modified_embeddings = []
-    all_indexes = [j for j in range(len(embedding))]
-    # Set the seed for the np.random.normal(...) function
-    np.random.seed(int(time.time()))
-    for i in range(num_of_molecules):
 
-        # Calculate the indexes of the num elements to be modified
-        random.shuffle(all_indexes)
-        indexes_to_be_modified = all_indexes[0:num]
+    # Initialize embedding to be returned
+    modified_embedding = copy.deepcopy(embedding)
 
-        # Initialize embedding to be returned
-        modified_embedding = copy.deepcopy(embedding)
+    for j in range(len(embedding)):
+        current_range = noise_intervals_lengths[j]
+        modified_embedding[j] += random.random()*current_range-current_range/2
 
-        for j in range(num):
-            current_range = noise_intervals_lengths[indexes_to_be_modified[j]]
-            modified_embedding[indexes_to_be_modified[j]] += random.random()*current_range-current_range/2
-        modified_embeddings.append(modified_embedding)
-
-    # Return the embeddings with noise addition
-    return modified_embeddings
+    # Return the embedding with noise addition
+    return modified_embedding
 
 # ------------------------------ readDistributions ------------------------------- #
 
@@ -187,6 +169,52 @@ def readDistributions():
     f.close()
     return
 
+# ------------------------------ readDataset ------------------------------- #
+
+def readDataset():
+    path1 = "..\\..\\dataset\\Commercial_MW\\Commercial_MWlower330.csv"
+    path2 = "..\\..\\dataset\\Commercial_MW\\Commercial_MW330-500-1.csv"
+    path3 = "..\\..\\dataset\\Commercial_MW\\Commercial_MW330-500-2.csv"
+    path4 = "..\\..\\dataset\\Commercial_MW\\Commercial_MWhigher500.csv"
+
+    # List of input smiles strings
+    dataset_smiles = []
+
+    # Open the files
+    f1 = open(path1,"r")
+    f2 = open(path2,"r")
+    f3 = open(path3,"r")
+    f4 = open(path4,"r")
+
+    print("Start reading all the smiles...")
+
+    # Read all the smiles from f1
+    i=0
+    for x in f1:
+        dataset_smiles.append(x.replace("\n",""))
+        i += 1
+
+    # Read all the smiles from f2
+    for x in f2:
+        dataset_smiles.append(x.replace("\n",""))
+        i += 1
+
+    # Read all the smiles from f3
+    for x in f3:
+        dataset_smiles.append(x.replace("\n",""))
+        i += 1
+
+    # Read all the smiles from f4
+    for x in f4:
+        dataset_smiles.append(x.replace("\n",""))
+        i += 1
+
+    #close the files
+    f1.close()
+    f2.close()
+    f3.close()
+    f4.close()
+    return dataset_smiles
 
 # ------------------------------- Code ------------------------------- #
 
@@ -197,42 +225,72 @@ if __name__ == '__main__':
     # Read from distributions file
     readDistributions()
 
+    # Read from dataset
+    dataset_smiles = readDataset()
+
     # Specify the directory of the trained model
     model_dir = "..\\..\\model"
 
-    # List of input smiles strings
-    input_smiles = ["COc1ccc(OS(=O)(=O)C(F)(F)F)c(Br)c1"]
+    # Specify the path for the file containing dataset fingerprints
+    fp_filename = '..\\..\\dataset\\Commercial_MW\\fingerprints.h5'
 
-    print(f"Encoded: {input_smiles}","\n")
+    # Input smile
+    input_smile = "COc1ccccc1N2CCN(CC2)C(=O)c3ccc(nc3)c4cccs4"
+    in_fps = AllChem.RDKFingerprint(Chem.MolFromSmiles(input_smile)) 
+
+    print(f"Encoded: ",input_smile,"\n")
 
     # Using the model at model_dir path
     with load_model_from_directory(model_dir) as model:
         print()
 
         # Number of molecules to be generated from each input smiles string
-        num_of_molecules_to_generate = 10
+        num_of_molecules_to_generate = 20
+        # Starting value for the area
+        area = 0.01
+        # Weights for finding/not finding duplicates
+        present = 0.01
+        not_present = -0.05
 
         print("Start encoding...")
         # Process latent vector for each input smiles string
-        embeddings = model.encode(input_smiles)
+        embedding = (model.encode([input_smile]))[0]
 
         # Calculate num_of_molecules_to_generate diverse molecules for each input smiles string,
         # adding noise to the embeddings
-        list_of_embeddings = []
+        list_of_decoded_smiles = []
         print("Start adding noise...")
-        list_of_embeddings = addNoise(embeddings[0],0.2,num_of_molecules_to_generate)
+        while len(list_of_decoded_smiles) < num_of_molecules_to_generate and area < 0.5:
+            modified_molecule = (model.decode([addNoise(embedding,area)]))[0]
+            if modified_molecule in list_of_decoded_smiles:
+                area += present
+            else:
+                list_of_decoded_smiles.append(modified_molecule)
+                area += not_present
 
-        # Decode without a scaffold constraint
-        print("Start decoding...")
-        list_of_decoded_smiles = model.decode(list_of_embeddings)
+    # The first element is always the input molecule
+    list_of_decoded_smiles = list_of_decoded_smiles[1:len(list_of_decoded_smiles)]
 
-    # Compute fingerprints
-    print("Generating fingerprints...")
-    in_fps = [AllChem.RDKFingerprint(Chem.MolFromSmiles(x)) for x in input_smiles]
-    out_fps = [AllChem.RDKFingerprint(Chem.MolFromSmiles(x)) for x in list_of_decoded_smiles]
-
-    # Calculate similarity
-    for i in range(len(list_of_decoded_smiles)):
-        print("Input molecule: {}\nOutput molecule: {}\nSimilarity: {}\n".format(input_smiles[0], list_of_decoded_smiles[i], DataStructs.TanimotoSimilarity(in_fps[0], out_fps[i])))
-
-    print("Have a nice day :)")
+    # Similarity search
+    similarity_dataset = 0.5
+    num_of_similar_smiles_per_decoded_smiles = 5
+    output_smiles = []
+    fpe = FPSim2Engine(fp_filename)
+    for decoded_smile in list_of_decoded_smiles:
+        results = fpe.similarity(decoded_smile, similarity_dataset, n_workers=2)
+        results_smiles = []
+        for res in results:
+            if all(dataset_smiles[res[0]-1] != out_smi[0] for out_smi in output_smiles) and dataset_smiles[res[0]-1] != input_smile:
+                results_smiles.append(dataset_smiles[res[0]-1])
+                if len(results_smiles) >= num_of_similar_smiles_per_decoded_smiles:
+                    break
+        if len(results_smiles) == 0:
+            continue
+        res_fps = [AllChem.RDKFingerprint(Chem.MolFromSmiles(res_smi)) for res_smi in results_smiles]
+        similarities = [DataStructs.TanimotoSimilarity(in_fps, out_fps) for out_fps in res_fps]
+        output_smiles.append([results_smiles[similarities.index(max(similarities))], max(similarities)])
+        
+    output_smiles.sort(key=lambda x : x[1],reverse=True)
+    # Print ouput
+    for i in range(len(output_smiles)):
+        print("Input molecule: {}\nOutput molecule: {}\nSimilarity: {}\n".format(input_smile, output_smiles[i][0], output_smiles[i][1]))
