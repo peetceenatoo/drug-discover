33af894a461619ea20cdfeab5ffc9192b52acad9
Mattia <piccinatomattia@gmail.com>
2023-06-21 11:17:14 +0200

    Push overview of exploration data

---
+++ data analysis/exploration/overview.py
@@ -0,0 +1,67 @@
+
+# Specify the paths for the database
+path1 = "noise_quality.txt"
+path2 = "project_quality.txt"
+
+print("# ----------- Let's compute data about the noise ----------- #")
+
+# Open the first file
+f = open(path1,"r")
+
+# To compute average number of outputs
+input_count = 0
+output_count = 0
+
+# To compute counters
+input_quality = {}      # temp   
+counters = {}           # counters
+
+# Read the ranges for each feature and put them in the rows map
+for row in f:
+    # Remove leading/trailing whitespace and newline characters
+    row = row.strip()
+    
+    # New input string
+    if row and row[0] == "I":
+        current_string = row.split(" ")[1]
+        print("Current string: {}".format(current_string))
+        input_count += 1
+
+    # New output string (save its quality)
+    elif row and row[0] == "O":
+        output_count += 1
+        input_quality[row.split(" ")[1]] = float(row.split(" ")[2])
+
+    # If done with the current input string, calculate quantities for current input string
+    else:
+        counters[current_string] = {}
+        for x in [0.9, 0.7, 0.5]:
+            counters[current_string][x] = 0
+        for output in input_quality:
+            if input_quality[output] >= 0.9:
+                counters[current_string][0.9] += 1
+            elif input_quality[output] >= 0.7:
+                counters[current_string][0.7] += 1
+            elif input_quality[output] >= 0.5:
+                counters[current_string][0.5] += 1
+        print("Above 0.9 there are {} mols".format(counters[current_string][0.9]))
+        print("Above 0.7 there are {} mols".format(counters[current_string][0.7]))
+        print("Above 0.5 there are {} mols".format(counters[current_string][0.5]))
+        print()
+        input_quality = {}
+
+# Final overview
+print("Average number of outputs for each input mol: {}".format(output_count/input_count))
+sum = {}
+for x in [0.9, 0.7, 0.5]:
+    sum[x] = 0
+for input in counters:
+    sum[0.5] += counters[input][0.5]
+    sum[0.7] += counters[input][0.7]
+    sum[0.9] += counters[input][0.9]
+print("Average above 0.9 for each input mol: {}".format(sum[0.9]/input_count))
+print("Average above 0.7 for each input mol: {}".format(sum[0.7]/input_count))
+print("Average above 0.5 for each input mol: {}".format(sum[0.5]/input_count))
+
+# Close the file
+f.close()
